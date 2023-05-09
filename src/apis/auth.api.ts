7ef630835159d94b0ab47b34d3535658e486aed0
Gia Hào Nguyễn <giahao070901@gmail.com>
2023-05-09 22:18:36 +0700

    fix typeerror

--- src/apis/auth.api.ts
+++ src/apis/auth.api.ts
@@ -1,15 +1,14 @@
 import { AuthResponse, LogoutResponse } from 'src/types/auth.type'
-import { SuccessResponse } from 'src/types/utils.type'
 import http from 'src/utils/http'
 
 const authApi = {
   registerAccount: (body: { email: string; password: string }) => {
     const url = '/register'
-    return http.post<SuccessResponse<AuthResponse>>(url, body)
+    return http.post<AuthResponse>(url, body)
   },
   loginAccount: (body: { email: string; password: string }) => {
     const url = '/login'
-    return http.post<SuccessResponse<AuthResponse>>(url, body)
+    return http.post<AuthResponse>(url, body)
   },
   logoutAccount: () => {
     const url = '/logout'
