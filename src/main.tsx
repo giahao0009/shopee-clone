2ce7a6377c66fbf8118168572aac1731f4647806
Gia Hào Nguyễn <giahao070901@gmail.com>
2024-01-17 12:06:05 +0700

    change base

--- src/main.tsx
+++ src/main.tsx
@@ -22,7 +22,7 @@ const queryClient = new QueryClient({
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
-      <BrowserRouter>
+      <BrowserRouter basename='/shopee-clone'>
         <AppProvider>
           <App />
           <ToastContainer />
