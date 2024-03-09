8ed729b2027be9ffeae99045e135deb3aaa23b71
Nguyen Gia Hao <75575801+giahao0009@users.noreply.github.com>
2024-03-09 22:03:42 +0700

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -22,7 +22,7 @@ const queryClient = new QueryClient({
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
-      <BrowserRouter basename='/shopee-clone'>
+      <BrowserRouter>
         <AppProvider>
           <App />
           <ToastContainer />
